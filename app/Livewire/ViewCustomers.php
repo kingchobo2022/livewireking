d682a4bb49276a412a874612cd352c595ceae821
kingchobo <kingchobo2022@gmail.com>
2024-11-25 07:58:40 +0900

    5th customer view

--- app/Livewire/ViewCustomers.php
+++ app/Livewire/ViewCustomers.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class ViewCustomers extends Component
 {
+    public $customer = [];
+    public function mount($id) {
+        $this->customer = User::find($id);
+    }
     public function render()
     {
         return view('livewire.view-customers');
--- resources/views/livewire/view-customers.blade.php
+++ resources/views/livewire/view-customers.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div class="card offset-3 col-6">
+    <h5 class="card-header">고객 조회</h5>
+    <div class="card-body">
+        <h5 class="card-title">아이디 : {{ $customer->id }}</h5>
+        <h5 class="card-title">이름 : {{ $customer->name }}</h5>
+        <h5 class="card-title">이메일 : {{ $customer->email }}</h5>
+        <h5 class="card-title">전화 : {{ $customer->phone }}</h5>
+        <button wire:navigate href="/customers" class="btn btn-secondary">Back</button>
+    </div>
 </div>
